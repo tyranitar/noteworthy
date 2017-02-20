c4df0d30e3148cfd07509ad057c2dbf31f4626e4
Allen Chan <allench12345@gmail.com>
2017-02-19 22:28:58 -0500

    Adding webpack config and preliminary views

---
+++ public/views/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+export default class Layout extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+      console.log("fuck?");
+      return (<div>Sup buddy</div>)
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -7,29 +7,13 @@ var babelLoader = {
     loader  : 'babel-loader',
 
     query: {
-        presets : ['react', 'es2015', 'stage-0'],
-        plugins : ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+        presets : ['react', 'es2015']
     }
 };
 
-var plugins = [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-        mangle    : false,
-        sourcemap : false,
-        compress: {
-            warnings: false
-        }
-    }),
-    new webpack.DefinePlugin({ "global.GENTLY": false })
-];
-
 // Not transpiling server code since everything breaks.
 module.exports = { // Client.
-    context : path.join(__dirname, 'src', 'public'),
-    devtool : debug ? 'eval' : null,
+    context : path.join(__dirname, '.', 'public'),
     entry: [
         './client.js'
     ],
@@ -42,15 +26,8 @@ module.exports = { // Client.
         loaders: [ babelLoader ]
     },
 
-    devServer: {
-        port: 8082
-    },
-
     output: {
-        path     : path.join(__dirname, 'src/public'),
-        publicPath: 'src/public',
+        path     : path.join(__dirname, 'public'),
         filename : 'client.min.js'
-    },
-
-    plugins: debug ? [] : plugins
+    }
 };
