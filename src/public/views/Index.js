9d8dd0bc1a401d7cc4dec8bb564161945feb1bcd
Allen Chan <allench12345@gmail.com>
2017-03-16 17:02:19 -0400

    making more UI fixes

--- src/public/views/Index.js
+++ src/public/views/Index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import Mic from 'material-ui/svg-icons/av/mic';
+import Stop from 'material-ui/svg-icons/av/stop';
 import FileUpload from 'material-ui/svg-icons/file/file-upload';
 import { grey500, tealA400, white } from 'material-ui/styles/colors';
+import Recorder from 'react-recorder'
 
 const layoutStyle = {
     height: '100%',
@@ -48,8 +50,82 @@ const iconStyle = {
 export default class Index extends React.Component {
     constructor() {
         super();
-    }
 
+        this.state = {
+          playing: false 
+        }
+
+        this.onStop = (blob) => {
+          var blobUrl = URL.createObjectURL(blob);
+          var reader = new FileReader;
+            
+          reader.onload = function() {
+            var blobAsDataUrl = reader.result;
+            var base64 = blobAsDataUrl.split(',')[1];
+            var buf = new Buffer(base64, 'base64');
+            fs.writeFile("audio/test.mp3", buf, function(err) {
+              if(err) {
+                console.log("err", err);
+              } else { 
+              }
+            }) 
+          };
+            
+          reader.readAsDataURL(blob);
+        }
+
+        this.startRecorder = () => {
+          this.refs.Recorder.start();
+        }
+
+        this.start = () => {
+          this.state.node.connect(this.state.audioCtx.destination);
+          this.setState({playing: true});
+        }
+
+        this.stop = () => {
+          this.state.node.disconnect();
+          this.refs.Recorder.stop();
+          this.setState({playing: false})
+
+        }
+
+        this.getStream = (stream) => {
+
+          var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+          var analyser = audioCtx.createAnalyser();
+          var source = audioCtx.createMediaStreamSource(stream);
+          source.connect(analyser);
+          var javascriptNode = audioCtx.createScriptProcessor(2048, 1, 1);
+ 
+          analyser.smoothingTimeConstant = 0.3;
+          analyser.fftSize = 1024;
+       
+          analyser.connect(javascriptNode);
+
+          javascriptNode.onaudioprocess = function() {
+              var ctx = document.getElementById("mic_activity").getContext("2d");
+              var array =  new Uint8Array(analyser.frequencyBinCount);
+              analyser.getByteFrequencyData(array);
+              var values = 0,
+                length = array.length;
+       
+              for (var i = 0; i < length; i++) values += array[i];
+       
+              var average = values / length;
+              ctx.clearRect(0, 0, 30, 150);
+              var grad = ctx.createLinearGradient(1,1,28,148);
+              grad.addColorStop(0,"#FF0000");
+              grad.addColorStop(0.5, "yellow");
+              grad.addColorStop(1,"#00FF00");
+              ctx.fillStyle=grad;
+              ctx.fillRect(1,148-average,28,148);
+          }
+
+          this.setState({node: javascriptNode, audioCtx: audioCtx});
+          //javascriptNode.connect(audioCtx.destination);
+        }
+    }
     render() {
         return (
             <div style={ layoutStyle }>
@@ -58,10 +134,22 @@ export default class Index extends React.Component {
                         <span style={ headerStyle1 }>Note</span><span style={ headerStyle2 }>worthy</span>
                     </div>
 
+                    <canvas id="mic_activity" width="500" height="150"></canvas>
+
                     <div style={ containerStyle }>
-                        <FloatingActionButton style={ buttonStyle } iconStyle={ iconStyle } backgroundColor={ white }>
-                            <Mic />
-                        </FloatingActionButton>
+                        <Recorder ref='Recorder' onStop={this.onStop} blobOpts={{type: 'audio/mp3'}} onStart ={this.start} gotStream={this.getStream}/>
+
+                        {!this.state.playing &&
+                            <FloatingActionButton style={ buttonStyle } iconStyle={ iconStyle } backgroundColor={ white } onTouchTap={this.startRecorder}>
+                                <Mic />
+                            </FloatingActionButton>
+                        } 
+
+                        {this.state.playing && 
+                            <FloatingActionButton style={ buttonStyle } iconStyle={ iconStyle } backgroundColor={ white } onTouchTap={this.stop}>
+                                <Stop />
+                            </FloatingActionButton>
+                        }
 
                         <FloatingActionButton style={ buttonStyle } iconStyle={ iconStyle } backgroundColor={ white }>
                             <FileUpload />
