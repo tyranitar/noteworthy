d8d2e7a2f06f19a70d8b315e791451f1bd66822a
Tyron Jung <tyronjung@hotmail.com>
2017-03-17 14:55:57 -0400

    Use hash history (#23)

--- src/public/app.js
+++ src/public/app.js
@@ -1,20 +1,18 @@
-import { createHashHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, useRouterHistory } from 'react-router';
+import { Router, Route, hashHistory } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Index from './views/Index';
 
-const history = useRouterHistory(createHashHistory)();
 const app = document.getElementById('app');
 
 injectTapEventPlugin();
 
 ReactDOM.render(
     <MuiThemeProvider>
-        <Router history={ history }>
+        <Router history={ hashHistory }>
             <Route path='/' component={ Index }></Route>
         </Router>
     </MuiThemeProvider>,
