9640e3a5527aae49f330064774e85df31b74d7c8
chsophie <cheongsophie@gmail.com>
2017-03-18 17:49:30 -0400

    Fix indentation

--- src/octave/generate_unlabeled.m
+++ src/octave/generate_unlabeled.m
@@ -9,15 +9,14 @@ function unlabeled = generate_unlabeled(file_location)
 
     dt = 1/fs;
     t = linspace(0, length(y)/fs, length(y));
-	    % t = time vector
-	    % length(y)/fs = ending time
-	    % length(y) = lentgh of time vector
+		% t = time vector
+		% length(y)/fs = ending time
+		% length(y) = lentgh of time vector
 
     % plot(t, y);
     chunks = chunk_notes(y);
     	% A 2D array of [[start end], ... ] indeces
 
     % Insert FFT logic here
-    
 
 end
