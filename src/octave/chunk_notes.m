002d175eff1b8f5c632f50f0a3817387623a159c
Tyron Jung <tyronjung@hotmail.com>
2017-03-18 03:50:09 -0400

    Note chunking logic

---
+++ src/octave/chunk_notes.m
@@ -0,0 +1,63 @@
+function [start_indices, finish_indices] = chunk_notes(y)
+    num_samples = 500;
+    num_decay = 7500;
+    thresh_hi = 0.05;
+    thresh_lo = 0.05;
+
+    len = length(y);
+    y_abs = abs(y);
+
+    start_indices = [];
+    finish_indices = [];
+
+    finding_start = true; % Otherwise, finding the note finish point.
+
+    mean_val = mean(y_abs(1:num_samples));
+    mean_vals = zeros(len, 1); % Debug.
+    decay_count = 0;
+
+    for i = 1:(len - num_samples)
+        first_val = y_abs(i);
+        last_val = y_abs(i + num_samples);
+        mean_val = mean_val + (last_val - first_val) / num_samples;
+
+        mean_vals(i) = mean_val; % Debug.
+
+        if finding_start
+            if mean_val > thresh_hi
+                start_indices = vertcat(start_indices, i);
+                finding_start = false;
+            end
+        else
+            if mean_val < thresh_lo
+                decay_count = decay_count + 1;
+
+                if decay_count > num_decay
+                    finish_indices = vertcat(finish_indices, i + num_samples);
+                    finding_start = true;
+                    decay_count = 0;
+                end
+            elseif decay_count > 0
+                decay_count = 0;
+            end
+        end
+    end
+
+    if length(start_indices) > length(finish_indices)
+        % Pop the last one, since it has no corresponding finish index.
+        start_indices = start_indices(1:end - 1);
+    end
+
+    hold; % Debug.
+    plot(y_abs); % Debug.
+    plot(mean_vals, 'g'); % Debug.
+
+    % Debug
+    for i=1:length(start_indices)
+        start_index = start_indices(i);
+        finish_index = finish_indices(i);
+
+        vert_line(start_index, 1, 'r');
+        vert_line(finish_index, 1, 'm');
+    end
+end
