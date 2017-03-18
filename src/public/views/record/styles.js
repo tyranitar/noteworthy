2886b4d82afea8bec8e824f871d71999b160cf81
Allen Chan <allench12345@gmail.com>
2017-03-18 01:30:08 -0400

    fixing spaces

--- src/public/views/record/styles.js
+++ src/public/views/record/styles.js
@@ -14,70 +14,70 @@ const styles = {
 	
 	'btnSmallAudio': {
 		margin: '0px',
-	    padding: '5px',
-	    position: 'absolute',
-	    left: '70%',
-	    border: `2px solid ${ tealA400 }`
+		padding: '5px',
+		position: 'absolute',
+		left: '70%',
+		border: `2px solid ${ tealA400 }`
 	},
 
 	recordTitle: {
 		fontWeight: 'bold',
-        fontSize: '75px',
-        color: grey500,
-        textAlign: 'center'
+    	fontSize: '75px',
+    	color: grey500,
+    	textAlign: 'center'
 	},
 
 	successMessage: {
 		fontWeight: 'bold',
-        fontSize: '20px',
-        color: grey500,
-        textAlign: 'center'
+    	fontSize: '20px',
+    	color: grey500,
+    	textAlign: 'center'
 	},
 
 	recordCaption: {
 		fontWeight: 'bold',
-        fontSize: '40px',
-        color: grey500,
-        textAlign: 'center'
+    	fontSize: '40px',
+    	color: grey500,
+    	textAlign: 'center'
 	},
 
 	'iconSmall': {
 		width: '25px',
-    	height: '25px',
+   		height: '25px',
     	fill: tealA400
 	},
 
 	'btnMed': {
 		marginTop: `${ marginMedium }px`,
-        marginLeft: `${ marginMedium }px`,
-        marginRight: `${ marginMedium }px`,
-        width: `${ buttonSizeMedium }px`,
-        height: `${ buttonSizeMedium }px`,
-        padding: `${ paddingMedium }px`,
+    	marginLeft: `${ marginMedium }px`,
+    	marginRight: `${ marginMedium }px`,
+    	width: `${ buttonSizeMedium }px`,
+    	height: `${ buttonSizeMedium }px`,
+    	padding: `${ paddingMedium }px`,
 		margin: '25px',
-	    border: `5px solid ${ tealA400 }`
+		border: `5px solid ${ tealA400 }`
 	},
 
 	'iconMed': {
 		width: '50px',
-	    height: '50px',
-	    fill: tealA400
+		height: '50px',
+		fill: tealA400
 	},
 
 	'btnLarge': {
 		marginTop: `${ marginLarge }px`,
-        marginLeft: `${ marginLarge }px`,
-        marginRight: `${ marginLarge }px`,
-        width: `${ buttonSizeLarge }px`,
-        height: `${ buttonSizeLarge }px`,
-        padding: `${ paddingLarge }px`,
+    	marginLeft: `${ marginLarge }px`,
+    	marginRight: `${ marginLarge }px`,
+    	width: `${ buttonSizeLarge }px`,
+    	height: `${ buttonSizeLarge }px`,
+    	padding: `${ paddingLarge }px`,
 		margin: '25px',
-	    border: `5px solid ${ tealA400 }`
+		border: `5px solid ${ tealA400 }`
 	},
 
 	'iconLarge': {
 		width: '100px',
-    	height: '100px',
+   		height: '100px',
     	fill: tealA400
 	},
 
@@ -90,12 +90,12 @@ const styles = {
 
     'audioCanvas': {
     	'paddingLeft': 0,
-	    'paddingRight': 0,
-	    'marginLeft': 'auto',
-	    'marginRight': 'auto',
-	    'display': 'block',
-	    'width': '500px',
-	    'height': '150px'
+	 	'paddingRight': 0,
+		'marginLeft': 'auto',
+		'marginRight': 'auto',
+		'display': 'block',
+		'width': '500px',
+		'height': '150px'
     }
 }
 
