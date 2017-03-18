4b87be08219ec99f108b866971ca1410504fe3a6
Tyron Jung <tyronjung@hotmail.com>
2017-03-18 03:51:30 -0400

    Generate unlabeled setup

--- src/octave/generate_unlabeled.m
+++ src/octave/generate_unlabeled.m
@@ -6,4 +6,12 @@ function unlabeled = generate_unlabeled(file_location)
     % Chunk notes then FFT, keeping track of the timestamps for each note/chord.
     % Simply return the generated input vector, don't save anything.
     [y, fs] = audioread(file_location);
+    delta_t = 1 / fs;
+    t = (0:delta_t:((length(y) - 1) * delta_t))';
+
+    % Debug.
+    t = t(1:160000);
+    y = y(1:160000);
+
+    [start_indices, finish_indices] = chunk_notes(y);
 end
