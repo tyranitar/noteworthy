8c8ac77fee628dc9b497fa3ca36d515fbc1bb212
Allen Chan <allench12345@gmail.com>
2017-03-19 00:17:37 -0400

    Adding download functionality

---
+++ src/public/views/sheet/styles.js
@@ -0,0 +1,46 @@
+import { grey500, tealA400, white } from 'material-ui/styles/colors';
+
+export default {
+	cardTitle: {
+        fontWeight: 'bold',
+        fontSize: '25px',
+        color: 'black',
+        margin: 'auto'
+    },
+
+    layoutStyle: {
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center'
+    },
+
+    cardStyle: {
+        padding: '5px', 
+        width: '60%', 
+        margin: 'auto'
+    },
+
+    cardHeaderTitle: {
+    	textAlign: 'center'
+    },
+
+    downloadIconSmall: {
+        width: '25px',
+        height: '25px'
+    },
+
+    downloadBtnSmall: {
+        margin: '0px',
+        padding: '5px'
+    },
+
+    cardContainer: {
+    	width: '100%',
+    	backgroundColor: 'white',
+    	height: '500px'
+    },
+
+    sheetContainer: {
+    	height: '20%'
+    }
+}
