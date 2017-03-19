e998b3e3d560a553d0e511281ab179d088f9b897
chsophie <cheongsophie@gmail.com>
2017-03-18 21:47:36 -0400

    Removed testing code

--- src/octave/generate_unlabeled.m
+++ src/octave/generate_unlabeled.m
@@ -4,7 +4,7 @@ function unlabeled = generate_unlabeled(file_location)
     unlabeled = [];
 
     % Chunk notes then FFT, keeping track of the timestamps for each note/chord.
-    % Simply return the generated input vector, dont save anything.
+    % Simply return the generated input vector, don't save anything.
     [y, fs] = audioread(file_location);
 
     dt = 1/fs;
@@ -15,17 +15,6 @@ function unlabeled = generate_unlabeled(file_location)
 
     % plot(t, y);
     chunks = chunk_notes(y);    % A 2D array of [[start end], ... ] indeces
-
-    start_note = 1; % Which note you want to analyze
-    end_note = 3;   % Which note you want to stop the analysis at (inclusive)
-
-    first_start = chunks(start_note, 1);    % Set start index
-    third_end = chunks(end_note, 2);    % Set end index
-
-    t_three_note = t(first_start:third_end);    % Get time vector of the notes to be analyzed
-    y_three_note = y(first_start:third_end);    % Get data vector of the notes to be analyzed
-
-    plot(t_three_note, y_three_note);   % Plot notes to be analyzed
 
     % Insert FFT logic here
 
