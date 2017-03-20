98f2a59562a69e7f3772a2b52ed785f28e0d9dda
Tyron Jung <tyronjung@hotmail.com>
2017-03-20 09:46:50 -0400

    Load weights

--- src/octave/test_algorithm.m
+++ src/octave/test_algorithm.m
@@ -1,18 +1,8 @@
 function test_algorithm()
     more off;
 
-    data_dir = '../../data_test/';
-    unlabeled_dir = strcat(data_dir, 'unlabeled/');
-    labeled_dir = strcat(data_dir, 'labeled/');
-    weights_dir = '../../data/weights/';
-
-    Theta1_path = strcat(weights_dir, 'theta_1.mat');
-    Theta2_path = strcat(weights_dir, 'theta_2.mat');
-
-    Theta1 = load(Theta1_path);
-    Theta2 = load(Theta2_path);
-
     [X, y] = get_shuffled_examples(false);
+    [Theta1, Theta2] = load_weights();
 
     p = predict(Theta1, Theta2, X);
     accuracy = compare_p_y(p, y);
