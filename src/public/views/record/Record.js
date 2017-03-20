27e3d2b9d7a1f224b3bf2fbcdfdd7d08e136cb1a
Allen Chan <allench12345@gmail.com>
2017-03-19 23:45:30 -0400

    line

--- src/public/views/record/Record.js
+++ src/public/views/record/Record.js
@@ -14,6 +14,7 @@ import styles from './styles';
 import sharedStyles from '../../styles/index';
 import Snackbar from 'material-ui/Snackbar';
 
+
 const mediumIconProps = {
 	style: styles.btnMed, 
 	iconStyle: styles.iconMed, 
